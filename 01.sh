2386d794f212ab15cc15a047534ba58b32cae605
notebook_home_hp <notebook_home_hp@163.com>
2019-09-11 22:44:17 +0800

    add file edit commands

--- 01.sh
+++ 01.sh
@@ -2,10 +2,14 @@
 #
 #linux常用的基本命令详解
 
-ls #列出 -la -dl
-pwd #显示当前位置
-cd #切换目录 ~家目录 - 与上一个目录来回切换
-mkdir #创建目录 -p 创建子目录
+#ls #列出 -la -dl
+#pwd #显示当前位置
+#cd #切换目录 ~家目录 - 与上一个目录来回切换
+#mkdir #创建目录 -p 创建子目录
 #cp # -a -r -R --recureses
 #rm -rf
+# useradd -s -g -G
+# groupadd 
+# chmod u+x a+x 
+#echo "xxxx" | passwd --standin USERNAME
 
